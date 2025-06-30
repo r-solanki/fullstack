cf5cbd7e3a746521679b5fb13d754a82b548ec13
Riya Solanki <riya.solanki@utexas.edu>
2025-06-29 22:03:27 -0500

    1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,33 +1,34 @@
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-       <Header course={course}/>
-       <Content parts={parts}></Content>
-      <Total parts={parts}/>
+        <Header name={course.name}/>
+        <Content parts={course.parts}></Content>
+        <Total parts={course.parts}/>
     </div>
   )
 }
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
   )
 }
 
